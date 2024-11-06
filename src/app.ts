28f03e842f8c0170ea900556a1105f50f8661546
Vlad <Smirnov.ru92@mail.ru>
2024-11-06 10:50:18 +0500

    base example, one user

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,51 @@
-import express from 'express';
+import express, {json} from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
+import cors from 'cors'
 
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+app.use(json());
+app.use(cors());
 
-app.get('/', (req, res) => {
+const messages = [
+    { message: "Hello Vlad", id: "23f2332", user: { id: "sddsdsfds", name: "Dima" }},
+    { message: "Hello Dima", id: "23fd32c23", user: { id: "eefw2", name: "Vlad" }}
+]
+
+
+
+const server = http.createServer(app);
+const socketApp = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    }
+})
+
+
+
+
+app.get('/',cors(),(req, res) => {
     res.send("Hello, it's a WebSocket server!");
 });
 
-io.on('connection', (socket) => {
-    console.log('A user connected');
+
+socketApp.on('connection' ,(connection) => {
+    connection.on("client-message-sent" ,(message) => {
+        console.log(message);
+        const messageItem = {message,id:new Date().getMilliseconds().toString(),user: { id: "eefw2", name: "Vlad" }}
+
+        messages.push(messageItem)
+        console.log(messages)
+
+        connection.emit('new-message-sent', messageItem)
+
+    });
+
+    connection.emit("init-messages-published" ,messages);
 });
+
+
 
 server.listen(3009, () => {
     console.log('Listening on *:3000');
